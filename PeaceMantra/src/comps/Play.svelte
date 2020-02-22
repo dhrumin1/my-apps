dae487b4901e4b01bf5b3e04089c8c6fc0b11595
DJ <dj@dhaval.local>
2020-02-23 01:31:40 +0530

    fix play issue

--- PeaceMantra/src/comps/Play.svelte
+++ PeaceMantra/src/comps/Play.svelte
@@ -1,43 +1,46 @@
 <script>
-    import { Sound, CountDown, StartPauseStop } from './../configs/Store';
-    let mySound, myCountDown;
-	const unsubscribeSound = Sound.subscribe(value => {
-		mySound = value; 
-	});
-    const unsubscribeCount = CountDown.subscribe(value => {
-		myCountDown = value;
-	});
-    const unsubscribeStart = StartPauseStop.subscribe(value => {
-		takeAction(value);
-        //console.log(value);
-	});
-    
-    function takeAction(action){
-        let audioElm = document.getElementById("audio");
-        if(audioElm){
+  import { Sound, CountDown, StartPauseStop } from "./../configs/Store";
+  let mySound, myCountDown;
+  const unsubscribeSound = Sound.subscribe(value => {
+    mySound = value;
+  });
+  const unsubscribeCount = CountDown.subscribe(value => {
+    myCountDown = value;
+  });
+  const unsubscribeStart = StartPauseStop.subscribe(value => {
+    takeAction(value);
+    //console.log(value);
+  });
 
-        switch (action) {
-            case 'start','resume':
-                audioElm.play();
-                break;
-        case 'stop':
-                audioElm.stop();
-                break;
-        case 'pause':
-                audioElm.pause();
-                break;
-            default:
-                break;
-        }
-        }
+  function takeAction(action) {
+    let audioElm = document.getElementById("audio");
+    if (audioElm) {
+      switch (action) {
+        case "start":
+        case "resume":
+          audioElm.play();
+          break;
+        case "stop":
+          audioElm.pause();
+          audioElm.currentTime = 0;
+          break;
+        case "pause":
+          audioElm.pause();
+          break;
+        default:
+          break;
+      }
     }
+  }
 </script>
+
 <div>
-    {mySound.Name}<br/>
-    {#if mySound.Audio}
-    <audio id="audio" controls>
-        <source src={mySound.Audio} />
+  {mySound.Name}
+  <br />
+  {#if mySound.Audio}
+    <audio id="audio" src={mySound.Audio} controls>
+
     </audio>
-    {/if}
-    {myCountDown}
+  {/if}
+  {myCountDown}
 </div>
